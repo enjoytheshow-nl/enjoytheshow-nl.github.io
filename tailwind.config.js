bf591d94ed28661d1be6295fd7a7981459febf16
Vincent van Kekerix <vincent@vankekerix.nl>
2024-12-08 11:09:20 +0100

    Add build step to the project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'ets-blue': '#007eb9',
+        'ets-green': '#96c22f',
+        'ets-darkblue': '#15325a'
+      }
+    },
+  },
+  plugins: [],
+}
+
